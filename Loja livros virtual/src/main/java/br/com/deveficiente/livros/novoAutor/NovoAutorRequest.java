5403df2b8e2c2c9ee6bc73239f9f66b41e2a9996
JoseRomualdo <jromualdo3@hotmail.com>
2026-02-22 15:15:41 -0300

    feat(NovoAutorRequest): new file.

--- Loja livros virtual/src/main/java/br/com/deveficiente/livros/novoAutor/NovoAutorRequest.java
+++ Loja livros virtual/src/main/java/br/com/deveficiente/livros/novoAutor/NovoAutorRequest.java
@@ -1,5 +1,6 @@
 package br.com.deveficiente.livros.novoAutor;
 
+import br.com.deveficiente.livros.compartilhado.UniqueValue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -12,6 +13,7 @@ public class NovoAutorRequest {
 
     @NotBlank
     @Email
+    @UniqueValue(domainClass = Autor.class, fieldName = "email")
     private String email;
 
     @NotBlank
