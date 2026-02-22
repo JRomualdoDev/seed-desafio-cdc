9123e23eeb2ff6ef793fd6fb962ba3eed8d4f077
JoseRomualdo <jromualdo3@hotmail.com>
2026-02-22 15:04:53 -0300

    feat(Autor): new file.

---
+++ Loja livros virtual/src/main/java/br/com/deveficiente/livros/novoAutor/Autor.java
@@ -0,0 +1,67 @@
+package br.com.deveficiente.livros.novoAutor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 400)
+    private String descricao;
+
+    private LocalDateTime instanteCriacao = LocalDateTime.now();
+
+    public Autor(
+            @NotBlank String nome,
+            @NotBlank @Email String email,
+            @NotBlank String descricao
+    ) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+    }
+
+    public Autor() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", instanteCriacao=" + instanteCriacao +
+                '}';
+    }
+
+
+}
