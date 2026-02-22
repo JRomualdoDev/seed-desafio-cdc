b4068a0b08d34722177c5d19362abdbeea37aca3
JoseRomualdo <jromualdo3@hotmail.com>
2026-02-22 15:05:11 -0300

    feat(Livro): new file.

---
+++ Loja livros virtual/src/main/java/br/com/deveficiente/livros/novoLivro/Livro.java
@@ -0,0 +1,111 @@
+package br.com.deveficiente.livros.novoLivro;
+
+import br.com.deveficiente.livros.novaCategoria.Categoria;
+import br.com.deveficiente.livros.novoAutor.Autor;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    @Size(max = 500)
+    private String resumo;
+
+    private String sumario;
+
+    @NotNull
+    @Min(20)
+    private BigDecimal preco;
+
+    @NotNull
+    @Min(100)
+    private int numeroPaginas;
+
+    @NotBlank
+    private String isbn;
+
+    @NotNull
+    @Future
+    private LocalDate dataPublicacao;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+
+    public Long getId() {
+        return id;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public void setSumario(String sumario) {
+        this.sumario = sumario;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", resumo='" + resumo + '\'' +
+                ", sumario='" + sumario + '\'' +
+                ", preco=" + preco +
+                ", numeroPaginas=" + numeroPaginas +
+                ", isbn='" + isbn + '\'' +
+                ", dataPublicacao=" + dataPublicacao +
+                ", categoria=" + categoria +
+                ", autor=" + autor +
+                '}';
+    }
+}
