bbd1cc58d709179c87b17f6f3ad4193ae9cd15ac
JoseRomualdo <jromualdo3@hotmail.com>
2026-02-22 15:18:35 -0300

    feat(Uniquevalue): new file.

---
+++ Loja livros virtual/src/main/java/br/com/deveficiente/livros/compartilhado/UniqueValue.java
@@ -0,0 +1,23 @@
+package br.com.deveficiente.livros.compartilhado;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = {UniqueValueValidator.class})
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UniqueValue {
+
+    String message() default "{br.com.deveficiente.beanvalidation.uniqueValue}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String fieldName();
+
+    Class<?> domainClass();
+}
